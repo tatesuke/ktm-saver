f1094a74682e92bd31483260e10e62a7fccff40e
tatesuke <acousticdays@gmail.com>
2016-03-21 17:03:05 +0900

    foreverがカレントディレクトリを参照してうまく動かなかったのを修正

--- launch.js
+++ launch.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 
-var port = 56565;
+const DEFAULT_PORT = "56565";
+
+var port = DEFAULT_PORT;
 if (3 <= process.argv.length) {
 	port = process.argv[2];
 }
 
+console.log("listening on port " + port);
+
+var path = require('path');
+var sourceDir = path.dirname(process.argv[1]);
+
 var forever = require('forever');
-forever.startDaemon('ktmsaver.js', {args:[port]});
+forever.startDaemon('ktmsaver.js', {args:[port], sourceDir:sourceDir});
