6b821db93779decbfd2632368a896b744f871991
tatesuke <acousticdays@gmail.com>
2016-03-21 20:17:37 +0900

    上書き保存できた

--- browser_script/ktmSaver_browser.js
+++ browser_script/ktmSaver_browser.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name         ktmSaverForBrouser
-// @namespace    http://tatesuke.github.io/KanTanMarkdown/
+// @name         ktmSaverForBrowser
+// @namespace    https://github.com/tatesuke/ktmsaver
 // @version      0.1
 // @description  
 // @author       tatesuke
@@ -9,8 +9,6 @@
 // ==/UserScript==
 /* jshint -W097 */
 'use strict';
-
-var port = 8090;
 
 (function() {
     // ローカルファイル以外実行しない
@@ -25,10 +23,13 @@ var port = 8090;
         return;
     }
 
-    // 古いバージョンでは実行しない
+    // getHTMLForSave関数がないバージョンでは実行しない
     if (!getHTMLForSave) {
         return;
     }
+
+    // UIを構築
+    showKtmSaverUI();
 
     // オリジナルのkeydownイベントを退避させる
     var originalEventListener;
@@ -41,80 +42,173 @@ var port = 8090;
     }
     document.body.removeEventListener("keydown", originalEventListener);
 
-    var saveQueue = null; // キューをストック 
-    var savePreviewWait = 50;
     // 新しいkeydownイベントを定義
     document.body.addEventListener("keydown", function(event) {
         var code = (event.keyCode ? event.keyCode : event.which);
 
         if (!isDrawMode() && (code == 83) && (event.ctrlKey || event.metaKey)) {
-            // CTRL+Sで保存する
+            // CTRL+Sのみ新しい処理に書き換える
             event.preventDefault();
-
-            // イベント発生の都度、キューをキャンセル 
-            clearTimeout(saveQueue);
-
-            // waitで指定したミリ秒後に所定の処理を実行 
-            // 経過前に再度イベントが発生した場合
-            // キューをキャンセルして再カウント 
-            saveQueue = setTimeout(startOverwriteSave, savePreviewWait);
+            queueOverwriteSave();
             return false;
         } else {
+            // CTRL+Sはオリジナルそのまま使う
             originalEventListener(event);
         }
-
     });
 
-    function startOverwriteSave() {
+    function showKtmSaverUI() {
+        /* 上書き設定ボタン */
+        var ktmSaverMenuButton = document.createElement("button");
+        var settingButton = document.querySelector("#settingMenuButton");
+        ktmSaverMenuButton.innerHTML = "上書き設定";
+        ktmSaverMenuButton.style.height = settingButton.offsetHeight;
+        ktmSaverMenuButton.style.position = "absolute"
+        ktmSaverMenuButton.style.top = settingButton.offsetTop + "px";
+        ktmSaverMenuButton.style.left = (settingButton.offsetLeft + settingButton.offsetWidth) + "px";
+        on(ktmSaverMenuButton, "click", function() {
+            var button = this;
+            var menu = document.getElementById("ktmSaverMenu");
+            menu.style.top = (this.offsetTop + this.scrollHeight) + "px";
+            menu.style.left = this.offsetLeft + "px";
+            showBlock(menu);
+        });
+        document.body.appendChild(ktmSaverMenuButton);
 
-        var id = new Date().getTime();
+        /* 上書き設定メニュー */
+        var ktmSaverMenu = document.createElement("div");
+        ktmSaverMenu.classList.add("popupMenu");
+        ktmSaverMenu.id  = "ktmSaverMenu";
+        ktmSaverMenu.innerHTML =
+            '<li>ポート番号:<input type="text" id="ktmServerPort" value="' + getKtmSaverPort() + '">' +
+            '<button id="cliseKtmSaverMenuButton">閉じる</button></li>';
+        document.body.appendChild(ktmSaverMenu);
 
-        var body = document.body;
-        var tempIframe = document.createElement("iframe");
-        //tempIframe.style.display = "none";
-        body.appendChild(tempIframe);
+        on("#ktmServerPort", "change", function() {
+            localStorage.setItem("com.tatesuke.ktmsaver.port", this.value);
+        });
+        on("#cliseKtmSaverMenuButton", "click", function() {
+            hide(ktmSaverMenu);
+        });
 
-        var fbody = tempIframe.contentWindow.document.body;
+        /* 接続中メッセージ */
+        var connectingMessage = document.createElement("div");
+        connectingMessage.innerHTML = "接続中..";
+        connectingMessage.id = "ktmSaverConnectingMessage";
+        connectingMessage.style.width = "150px";
+        connectingMessage.style.height = (settingButton.offsetHeight - 2) + "px";
+        connectingMessage.style.position = "absolute";
+        connectingMessage.style.top = 0;
+        connectingMessage.style.left = ((document.body.offsetWidth / 2) - (150 / 2)) + "px"
+        connectingMessage.style.textAlign = "center";
+        connectingMessage.style.color = "#c09853";
+        connectingMessage.style.backgroundColor = "#fcf8e3";
+        connectingMessage.style.border = "1px solid #fbeed5";
+        connectingMessage.style.display = "none";
+        document.body.appendChild(connectingMessage);
 
-        var form = document.createElement("form");
-        form.method = "post";
-        form.action = "http://127.0.0.1:" + port + "/save";
-        form.style.display = "none";
-        fbody.appendChild(form);
+        /* 保存中メッセージ */
+        var savingMessage = document.createElement("div");
+        savingMessage.innerHTML = "保存中..";
+        savingMessage.id = "ktmSaverSavinggMessage";
+        savingMessage.style.width = "150px";
+        savingMessage.style.height = (settingButton.offsetHeight - 2) + "px";
+        savingMessage.style.position = "absolute";
+        savingMessage.style.top = 0;
+        savingMessage.style.left = ((document.body.offsetWidth / 2) - (150 / 2)) + "px"
+        savingMessage.style.textAlign = "center";
+        savingMessage.style.color = "#c09853";
+        savingMessage.style.backgroundColor = "#fcf8e3";
+        savingMessage.style.border = "1px solid #fbeed5";
+        savingMessage.style.display = "none";
+        document.body.appendChild(savingMessage);
 
-        var idElement = document.createElement("input");
-        idElement.type = "text";
-        idElement.name = "id";
-        idElement.value = id;
-        form.appendChild(idElement);
+        /* 保存完了メッセージ */
+        var sucessMessage = document.createElement("div");
+        sucessMessage.innerHTML = "保存完了";
+        sucessMessage.id = "ktmSaverSuccessMessage";
+        sucessMessage.style.width = "150px";
+        sucessMessage.style.height = (settingButton.offsetHeight - 2) + "px";
+        sucessMessage.style.position = "absolute";
+        sucessMessage.style.top = 0;
+        sucessMessage.style.left = ((document.body.offsetWidth / 2) - (150 / 2)) + "px"
+        sucessMessage.style.textAlign = "center";
+        sucessMessage.style.color = "#3a87ad";
+        sucessMessage.style.backgroundColor = "#d9edf7";
+        sucessMessage.style.border = "1px solid #bce8f1";
+        sucessMessage.style.display = "none";
+        document.body.appendChild(sucessMessage);
 
-        var filePathElement = document.createElement("input");
-        filePathElement.type = "text";
-        filePathElement.name = "filePath";
-        filePathElement.value = decodeURIComponent(location.href);
-        form.appendChild(filePathElement);
+        /* 保存失敗メッセージ */
+        var errorMessage = document.createElement("div");
+        errorMessage.innerHTML = "！保存失敗！";
+        errorMessage.id = "ktmSaverErrorMessage";
+        errorMessage.style.width = "150px";
+        errorMessage.style.height = (settingButton.offsetHeight - 2) + "px";
+        errorMessage.style.position = "absolute";
+        errorMessage.style.top = 0;
+        errorMessage.style.left = ((document.body.offsetWidth / 2) - (150 / 2)) + "px"
+        errorMessage.style.textAlign = "center";
+        errorMessage.style.color = "#b94a48";
+        errorMessage.style.backgroundColor = "#f2dede";
+        errorMessage.style.border = "1px solid #eed3d7";
+        errorMessage.style.display = "none";
+        document.body.appendChild(errorMessage);
+    }
 
-        var contentElement = document.createElement("input");
-        contentElement.type = "hidden";
-        contentElement.name = "content";
-        contentElement.value = getHTMLForSave();
-        form.appendChild(contentElement);
-
-
-        tempIframe.onload = function(e) {
-            console.timeEnd("hoge");
-            this.onload = null;
-            var script = document.createElement("script");
-            script.src = "http://127.0.0.1:" + port + "/check?id=" + id;
-            script.class = "kantanSaveScript";
-            script.onerror = function() {
-                alert("上書き保存に失敗しました。\n" +
-                    "クライアントアプリが起動しているか確かめてください。");
-            };
-            document.querySelector("#updateScriptArea").appendChild(script);
+    function getKtmSaverPort() {
+        var port = localStorage.getItem("com.tatesuke.ktmsaver.port");
+        if (!port) {
+            port = "56565";
         }
-        console.time("hoge");
-        form.submit();
+        return port;
+    }
+
+    
+    // 上書き保存
+    var overwriteSaveQueue = null;
+    var clearMessageQueue = null;
+    var queueWait = 50; 
+    function queueOverwriteSave() {
+        clearTimeout(overwriteSaveQueue);
+        clearTimeout(clearMessageQueue);
+        overwriteSaveQueue = setTimeout(doOverwriteSave, queueWait);
+    }
+
+    function doOverwriteSave() {
+        hide(document.querySelector("#ktmSaverSuccessMessage"));
+        hide(document.querySelector("#ktmSaverErrorMessage"));
+        showBlock(document.querySelector("#ktmSaverConnectingMessage"));
+
+        var data = {
+            filePath:decodeURIComponent(location.href.replace(/file:\/\/\//, "")),
+            content: getHTMLForSave()
+        };
+
+        var port = getKtmSaverPort();
+        var ws = new WebSocket('ws://localhost:' + port + '/');
+        ws.onopen = function() {
+            hide(document.querySelector("#ktmSaverConnectingMessage"));
+            showBlock(document.querySelector("#ktmSaverSavinggMessage"));
+            ws.send(JSON.stringify(data));
+        };
+
+        ws.onmessage = function(event) {
+            hide(document.querySelector("#ktmSaverSavinggMessage"));
+            showBlock(document.querySelector("#ktmSaverSuccessMessage"));
+            clearMessageQueue = setTimeout(function() {
+                hide(document.querySelector("#ktmSaverSuccessMessage"));
+            }, 3000)
+        };
+
+        ws.onerror = function() {
+            hide(document.querySelector("#ktmSaverConnectingMessage"));
+            hide(document.querySelector("#ktmSaverSavinggMessage"));
+            showBlock(document.querySelector("#ktmSaverErrorMessage"));
+            alert(data.filePath + "の保存に失敗しました。");
+            saved = false;
+            doPreview();
+        };
     }
 
 })();
