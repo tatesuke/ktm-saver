33275b8886cbb382e7a397a573ab1abcaa44d896
tatesuke <acousticdays@gmail.com>
2016-03-24 21:13:56 +0900

    バックアップ機能追加

--- src/main/java/com/tatesuke/ktmsaver/service/SaveService.java
+++ src/main/java/com/tatesuke/ktmsaver/service/SaveService.java
@@ -2,8 +2,10 @@ package com.tatesuke.ktmsaver.service;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.tatesuke.ktmsaver.dto.Request;
 import com.tatesuke.ktmsaver.dto.Response;
@@ -16,6 +18,7 @@ import com.tatesuke.ktmsaver.ui.KTMFileDialog;
  */
 public class SaveService {
 
+	private static final String BACKUPFILE_SYNBOL = "ktmbk";
 	private KTMFileDialog dialog = getKTMFileDialog();
 
 	protected KTMFileDialog getKTMFileDialog() {
@@ -86,20 +89,7 @@ public class SaveService {
 				response.filePath = null;
 				response.message = null;
 			} else {
-				try (FileOutputStream fo = new FileOutputStream(file)) {
-					
-					byte[] bytes = request.content.getBytes("utf-8");
-					fo.write(bytes);
-					
-					response.result = Response.Result.SUCCESS;
-					response.filePath = file.getAbsolutePath();
-					response.message = null;
-				} catch (IOException e) {
-					e.printStackTrace();
-					response.result = Response.Result.ERROR;
-					response.filePath = null;
-					response.message = e.getMessage();
-				}
+				save(request, response, file);
 			}
 
 			return response;
@@ -147,19 +137,83 @@ public class SaveService {
 			Response response = new Response();
 
 			File file = new File(request.fileDir, request.fileName);
-			try (FileWriter fw = new FileWriter(file)) {
-				fw.append(request.content);
-				response.result = Response.Result.SUCCESS;
-				response.filePath = file.getAbsolutePath();
-				response.message = null;
-			} catch (IOException e) {
-				response.result = Response.Result.ERROR;
-				response.filePath = null;
-				response.message = e.getMessage();
-			}
+			save(request, response, file);
 
 			return response;
 		}
 	}
 
+	private void save(Request request, Response response, File file) {
+		if (request.backupEnabled == true) {
+			File baseDir = new File(request.backupDir);
+			if (!baseDir.isAbsolute()) {
+				baseDir = new File(file.getParentFile(), request.backupDir);
+			}
+			backup(file, baseDir, request.backupGeneration);
+		}
+		try (FileOutputStream fo = new FileOutputStream(file)) {
+			byte[] bytes = request.content.getBytes("utf-8");
+			fo.write(bytes);
+
+			response.result = Response.Result.SUCCESS;
+			response.filePath = file.getAbsolutePath();
+			response.message = null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			response.result = Response.Result.ERROR;
+			response.filePath = null;
+			response.message = e.getMessage();
+		}
+	}
+
+	private void backup(File file, File backupDir, int backupGeneration) {
+		if (!file.isFile()) {
+			return;
+		}
+		if (!backupDir.isDirectory()) {
+			boolean mkdirs = backupDir.mkdirs();
+			if (mkdirs == false) {
+				throw new RuntimeException("バックアップディレクトリを作成できませんでした。");
+			}
+		}
+
+		List<File> backUpFileList = Stream
+				.of(backupDir.listFiles())
+				.filter(f -> f.isFile())
+				.filter(f -> f.getName().startsWith(file.getName()))
+				.filter(f -> {
+					String afterName = f.getName().substring(
+							file.getName().length());
+					return afterName.matches("\\." + BACKUPFILE_SYNBOL + "[1-9][0-9]*");
+				}).sorted((f1, f2) -> {
+					int n1 = getFileNumber(file, f1);
+					int n2 = getFileNumber(file, f2);
+					return n1 - n2;
+				}).collect(Collectors.toList());
+
+		if ((0 < backupGeneration)
+				&& (backupGeneration < (backUpFileList.size() + 1))) {
+			int deleteNum = backUpFileList.size() + 1 - backupGeneration;
+			for (int i = 0; i < deleteNum; i++) {
+				backUpFileList.get(i).delete();
+			}
+		}
+
+		int maxNumber = 0;
+		if (!backUpFileList.isEmpty()) {
+			File leatestFile = backUpFileList.get(backUpFileList.size() - 1);
+			maxNumber = getFileNumber(file, leatestFile);
+		}
+		int newNumber = maxNumber + 1;
+		String newName = file.getName() + "." + BACKUPFILE_SYNBOL + newNumber;
+		File newFile = new File(backupDir, newName);
+		file.renameTo(newFile);
+	}
+
+	private int getFileNumber(File baseFile, File file) {
+		int nameLength = baseFile.getName().length();
+		String afterName = file.getName().substring(nameLength);
+		String numberStr = afterName.substring(BACKUPFILE_SYNBOL.length() + 1);
+		return Integer.parseInt(numberStr);
+	}
 }
